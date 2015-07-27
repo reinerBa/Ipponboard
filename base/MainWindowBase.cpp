262e00be72325d370b3b5d01a535312330dfb633
Florian Mücke <fmuecke@gmail.com>
2015-07-27 22:10:42 +0200

    changed sound file from buzzer to gong (buzzer not working anymore)

--- base/MainWindowBase.cpp
+++ base/MainWindowBase.cpp
@@ -588,7 +588,7 @@ void MainWindowBase::read_settings()
 	settings.beginGroup(str_tag_Sounds);
 	{	
 		m_pController->SetGongFile(settings.value(str_tag_sound_time_ends,
-								"sounds/buzzer1.wav").toString());
+								"sounds/gong.wav").toString());
 	}
 	settings.endGroup();
 
